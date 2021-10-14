b8e1bb242b6ad6ec18c195bd2e273b11e85e6dc1
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-14 09:42:04 +0200

    a

--- src/main/java/se/systementor/Bil.java
+++ src/main/java/se/systementor/Bil.java
@@ -1,10 +1,14 @@
 package se.systementor;
 
-public class Bil {
+public class Bil { //KLASS = RITNING 
     private String regno; 
     private String modell; 
     private int year; 
+    private String color; 
   
+    public String getColor(){ return color;}    
+    public void setColor(String s){color = s; }
+    
     public String getModell(){ return modell;}    
     public void setModell(String s){modell = s; }
     public int getYear(){ return year;}    
